68cb598f3eb2e2dc5093e0e48879daa354ece1e4
Matheus <matheusmuruci@outlook.com>
2024-06-07 18:11:33 -0300

    Troca de titulo da aba do navegador

--- sample_spa/app/layout.tsx
+++ sample_spa/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My first Next SPA",
+  title: "Dositio",
   description: "My Task Manager",
 };
 
