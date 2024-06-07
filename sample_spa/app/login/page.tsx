6f1d4463220c366fe38fce8d8a9c2dfcdb977d43
Matheus <matheusmuruci@outlook.com>
2024-06-07 19:46:54 -0300

    feat[login]: Continuada a lógica para implementação da verificação de login

--- sample_spa/app/login/page.tsx
+++ sample_spa/app/login/page.tsx
@@ -4,36 +4,45 @@ import { AuthContext, SignIdData } from "@/context/AuthContext";
 import { useForm } from 'react-hook-form';
 import { useContext } from "react";
 
-const Login = async ({}) => {
-    const {register, handleSubmit} = useForm<SignIdData>();
+const Login = () => {
+    const { register, handleSubmit } = useForm<SignIdData>();
     const { login, authError } = useContext(AuthContext);
 
-    const handleLogin = async (data : SignIdData) => {
+    const handleLogin = async (data: SignIdData) => {
         await login(data);
     }
 
     return (
-        <div>
-            <form className="flex flex-col" onSubmit={handleSubmit(handleLogin)}>
-                <label htmlFor="username">Usuário: </label>
-                <input 
-                    {...register('username')}
-                    type="text" 
-                    name='username' 
-                    id='username' 
-                    placeholder="username" 
-                />
-                <label htmlFor="password">Senha: </label>
-                <input 
-                    {...register('password')}
-                    type="password" 
-                    name='password' 
-                    id='password' 
-                    placeholder="password" 
-                />
-                <input type="submit" value="Acessar" />
-            </form>
-            {authError && <p>{authError}</p>}
+        <div className="flex justify-center items-center h-screen">
+            <div className="w-full max-w-md p-6 bg-gray-100 rounded-lg shadow-md">
+                <form className="flex flex-col" onSubmit={handleSubmit(handleLogin)}>
+                    <label htmlFor="username" className="mb-2">Usuário:</label>
+                    <input
+                        {...register('username')}
+                        type="text"
+                        name='username'
+                        id='username'
+                        className="px-3 py-2 border rounded-md mb-4"
+                        placeholder="username"
+                    />
+                    <label htmlFor="password" className="mb-2">Senha:</label>
+                    <input
+                        {...register('password')}
+                        type="password"
+                        name='password'
+                        id='password'
+                        className="px-3 py-2 border rounded-md mb-4"
+                        placeholder="password"
+                    />
+                    <input
+                        type="submit"
+                        value="Acessar"
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 cursor-pointer"
+                    />
+                </form>
+                {/* Exibe erro de autenticação */}
+                {authError && <p className="text-red-500 mt-2">{authError}</p>}
+            </div>
         </div>
     );
 }
--- sample_spa/context/AuthContext.tsx
+++ sample_spa/context/AuthContext.tsx
@@ -21,37 +21,41 @@ type UserAuthentication = {
 
 export const AuthContext = createContext({} as AuthContextType);
 
-
-export default function AuthProvider( { children }: { children: React.ReactNode } ){
+export default function AuthProvider({ children }: { children: React.ReactNode }) {
     const [authError, setAuthError] = useState<string | null>(null);
 
     const router = useRouter();
 
-    async function login({username, password} : SignIdData) {
-        
-        let {'x-access-token': token} = await request<UserAuthentication>('http://127.0.0.1:3000/auth', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username, password}),
-            referrerPolicy: 'no-referrer',
-            cache: 'no-store'
-        });
+    async function login({ username, password }: SignIdData) {
+        try {
+            const { 'x-access-token': token } = await request<UserAuthentication>('http://127.0.0.1:3000/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password }),
+                referrerPolicy: 'no-referrer',
+                cache: 'no-store'
+            });
 
-        if(!token) setAuthError('Usuário ou senha inválidos. Verifique e tente novamente!');
-        else{
+            if (!token) {
+                setAuthError('Autenticação falhou. Por favor, verifique suas credenciais.');
+                return;
+            }
+
             setCookie(null, 'auth.token', token, {
                 maxAge: 60 * 60 * 1,
             });
-            
-            router.push('/tasks');
+
+            router.push('/product');
+        } catch (error) {
+            setAuthError('Ocorreu um erro ao tentar fazer login.');
         }
     }
-    
+
     return (
-        <AuthContext.Provider value={{login, authError}} >
+        <AuthContext.Provider value={{ login, authError }}>
             {children}
         </AuthContext.Provider>
     );
-};
+}
