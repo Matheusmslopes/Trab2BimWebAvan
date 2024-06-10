23a472b666c17abbab414df3ea58bba6d4855a33
Matheus <matheusmuruci@outlook.com>
2024-06-10 14:46:06 -0300

    feat[error]: Adicionado erro de usuário não encontrado

--- dositio-class/libs/error.js
+++ dositio-class/libs/error.js
@@ -3,6 +3,7 @@ export const AUTH_NO_TOKEN = createError('AUTH_NO_TOKEN', 'x-access-token is mis
 export const AUTH_INVALID_TOKEN = createError('AUTH_INVALID_TOKEN', 'The token provided is invalid', 401);
 
 export const NOT_FOUND = createError('NOT_FOUND', 'The requested resource could not be found', 404);
+export const USER_NOT_FOUND = createError('USER_NOT_FOUND', 'This user could not be found', 404);
 
 export const ALREADY_EXISTS = createError('ALREADY_EXISTS', 'This resource already exists in database', 412);
 
